35fa5571038de6502d83d370df55bf88f23397ee
Jason D. McCormick <jason@mfamily.org>
2023-05-10 17:07:48 -0400

    edge isn't at think anymore

--- index.php
+++ index.php
@@ -20,7 +20,6 @@ limitations under the License.
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 		<!-- <meta name="description" content="">
